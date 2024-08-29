6a0af7c24479614e591001700346aab9e092812b
That909kk <21086061.that@student.iuh.edu.vn>
2024-08-29 21:23:10 +0700

    week1 js 3_of_50_challenges_newbie_Part_01

--- 50_challenges_newbie_Part_01.js
+++ 50_challenges_newbie_Part_01.js
@@ -6,7 +6,10 @@ for (let i = 1; i <= 10; i++) {
 for (let i = 1; i < 100; i += 2) {
     console.log(i);
 }
-// 3. Print the multiplication table with 7 
+// 3. Print the multiplication table with 7
+for (let i = 1; i <= 10; i++) {
+    console.log(`7 * ${i} = ${7 * i}`);
+}
 // 4. Print all the multiplication tables with numbers from 1 to 10 
 // 5. Calculate the sum of numbers from 1 to 10 
 // 6. Calculate 10! 
