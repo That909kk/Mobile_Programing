14cb5f99d72dd41820a79be2e5bc20fea8732dc8
That909kk <21086061.that@student.iuh.edu.vn>
2024-08-29 21:07:24 +0700

    week1 js Part8_CodingChallenge_2

---
+++ Part8_CodingChallenge_2.js
@@ -0,0 +1,33 @@
+// Coding Challenge #2
+// Your tasks:
+// 1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+// by 1.6)
+// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+// converts it to km/h before storing the value, by multiplying the input by 1.6)
+// 4. Create a new car and experiment with the 'accelerate' and 'brake'
+// methods, and with the getter and setter.
+const CarCl = class {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+const Ford = new CarCl('Ford', 120);
+console.log(Ford.speedUS);
+Ford.accelerate();
+Ford.brake();
