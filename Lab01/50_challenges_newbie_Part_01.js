d2b15b79efbfb7835d18694b57021045f7f92ee1
That909kk <21086061.that@student.iuh.edu.vn>
2024-09-19 17:42:25 +0700

    week1 js 6_of_50_challenges_newbie_Part_01

--- Lab01/50_challenges_newbie_Part_01.js
+++ Lab01/50_challenges_newbie_Part_01.js
@@ -18,12 +18,17 @@
 //     }
 // }
 // 5. Calculate the sum of numbers from 1 to 10 
-    let sum = 0;
-    for (let i = 1; i <= 10; i++) {
-        sum += i;
-    }
-    console.log(sum);
+    // let sum = 0;
+    // for (let i = 1; i <= 10; i++) {
+    //     sum += i;
+    // }
+    // console.log(sum);
 // 6. Calculate 10! 
+let a = 1;
+for (let i = 1; i <= 10; i++) {
+    a*= i;
+}
+console.log(a);
 // 7. Calculate the sum of even numbers greater than 10 and less than 30 
 // 8. Create a function that will convert from Celsius to Fahrenheit 
 // 9. Create a function that will convert from Fahrenheit to Celsius 
