6b3642ede649f02eac5d4c5433326d7d883495dc
That909kk <21086061.that@student.iuh.edu.vn>
2024-09-19 17:39:06 +0700

    week1 js 4_of_50_challenges_newbie_Part_01

--- Lab01/50_challenges_newbie_Part_01.js
+++ Lab01/50_challenges_newbie_Part_01.js
@@ -1,16 +1,22 @@
 // 1. Print numbers from 1 to 10 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
 // 2. Print the odd numbers less than 100 
-for (let i = 1; i < 100; i += 2) {
-    console.log(i);
-}
+// for (let i = 1; i < 100; i += 2) {
+//     console.log(i);
+// }
 // 3. Print the multiplication table with 7
-for (let i = 1; i <= 10; i++) {
-    console.log(`7 * ${i} = ${7 * i}`);
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`7 * ${i} = ${7 * i}`);
+// }
 // 4. Print all the multiplication tables with numbers from 1 to 10 
+for (let i = 1; i <= 10; i++) {
+    console.log(`Bảng cửu Chương ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
 // 5. Calculate the sum of numbers from 1 to 10 
 // 6. Calculate 10! 
 // 7. Calculate the sum of even numbers greater than 10 and less than 30 
